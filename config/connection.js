fce57a6c02cb96e7763a216444eec03f63b4155a
subham1095 <79747574+subham1095@users.noreply.github.com>
2023-09-07 02:21:05 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,6 @@ module.exports = {
     // connection: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     // connection: `mongodb://127.0.0.1/${process.env.DB_NAME}`,
    //connection: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`,
-    // connection: `mongodb://atlas-sql-64b437e5157acd795ae0b02f-al1rm.a.query.mongodb.net/company_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`,
-    connection: `43.251.171.158/32/company_db`,
+    connection: `mongodb://atlas-sql-64b437e5157acd795ae0b02f-al1rm.a.query.mongodb.net/company_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`,
+    // connection: `43.251.171.158/32/company_db`,
 };
