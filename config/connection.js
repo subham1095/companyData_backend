f4d5a91009491cb19a31004648abdef34fdafefc
subham1095 <79747574+subham1095@users.noreply.github.com>
2023-09-07 01:20:45 +0530

    Update connection.js connected vercel with mongo

--- config/connection.js
+++ config/connection.js
@@ -2,5 +2,5 @@ module.exports = {
     // connection: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     // connection: `mongodb://127.0.0.1/${process.env.DB_NAME}`,
    //connection: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`,
-    connection: `mongodb+srv://vercel-admin-user:84nX6loCcMp2hb3B@cluster0.wxr8ov7.mongodb.net/`,
+    connection: `mongodb://atlas-sql-64b437e5157acd795ae0b02f-al1rm.a.query.mongodb.net/company_db?ssl=true&authSource=admin`,
 };
